c7cbf4c958d5d5bec66a152af4fc0cb0f6c11fd3
1712678498 <490217225@qq.com>
2021-05-11 17:07:23 +0800

    更新了 active 组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Active/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Active from './components/active.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Active
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/active.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    我是 火车王
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+</style>
